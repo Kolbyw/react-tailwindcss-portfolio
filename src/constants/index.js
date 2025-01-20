96dcf6f33e76d2cd2c0ab7b554e2ab1b9bf391c7
Kolby Wallin <kolbywallin@gmail.com>
2025-01-20 15:08:04 -0600

    Updating info

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,29 +4,16 @@ import siu from '../images/company/siu.jpg'
 import musicStreamer from '../images/musicStreamer.jpg'
 import rockPaperScissors from '../images/rockPaperScissors.jpg';
 import euplora from '../images/euplora.jpg';
-import Image1 from '../images/ui-project-1.jpg';
-import Image2 from '../images/web-project-2.jpg';
-import Image3 from '../images/mobile-project-2.jpg';
-import Image4 from '../images/mobile-project-1.jpg';
-import Image5 from '../images/web-project-1.jpg';
-import Image6 from '../images/ui-project-2.jpg';
-// Import icons
-import {
-	FiFacebook,
-	FiInstagram,
-	FiLinkedin,
-	FiTwitter,
-	FiYoutube,
-} from 'react-icons/fi';
+import iris from '../images/Iris.jpg';
 
 export const aboutMeData = [
 	{
 		id: 1,
-		bio: 'I am a junior at Southern Illinois University pursuing a major in computer science. I am very grateful to have multiple years of experience working as a software engineering intern at a fortune 50 company, State Farm. While working at State Farm I have vastly improved my technical skills, personal/professional skills, and have learned a number of new technologies, including AWS. I am a dedicated worker and a passionate learner and look forward to the challenges and experiences I have ahead of me.',
+		bio: 'I am a senior at Southern Illinois University pursuing a major in computer science. I am very grateful to have multiple years of experience working as a software engineering intern at a fortune 50 company, State Farm. While working at State Farm I have vastly improved my technical skills, personal/professional skills, and have learned a number of new technologies, including AWS. I am a dedicated worker and a passionate learner and look forward to the challenges and experiences I have ahead of me.',
 	},
 	{
 		id: 2,
-		bio: 'In my free time I love hanging out with friends, playing video games, and trying new hobbies. Some of these hobbies include weight lifting, soccer, golf, disc golf, rock climbing, and even learning french!',
+		bio: 'In my free time I love hanging out with friends, playing video games, and trying new hobbies. Some of these hobbies include weight lifting, soccer, golf, disc golf, rock climbing, and boxing!',
 	},
 ];
 
@@ -55,7 +42,7 @@ export const experiences = [
 		company_name: "State Farm",
 		icon: statefarm,
 		iconBg: "#383E56",
-		date: "June 2021 - August 2021",
+		date: "May 2021 - August 2021",
 		points: [
 			"• Developing a self-service Gitlab application using the MERN stack.",
 			"• Collaborating with product managers, developers, and other interns to create the application.",
@@ -81,12 +68,25 @@ export const experiences = [
 		company_name: "State Farm",
 		icon: statefarm,
 		iconBg: "#383E56",
-		date: "May 2022 - Present",
+		date: "May 2022 - August 2023",
 		points: [
 			"• Developing and maintaining web applications using a Vue.js front-end and Django framework back-end.",
-			"• Collaborating with development team to create and deliver a reliable, high-quality product.",
+			"• Collaborating with development team to create and deliver a reliable, high-quality geospatial mapping product.",
 			"• Implementing responsive features and pushing changes fixing user-submitted tickets.",
 			"• Utilizing Terraform to create infrastructure for an applications API using AWS resources.",
+			"• Achieved my AWS Certified Cloud Practitioner certification."
+		],
+	},
+	{
+		title: "Software Engineer Intern",
+		company_name: "State Farm",
+		icon: statefarm,
+		iconBg: "#383E56",
+		date: "May 2024 - Present",
+		points: [
+			"• Writing high quality frontend solutions in typescript with an Angular framework.",
+			"• Created a health check API that returns the health status of my teams application using AWS Lambda and API Gateway.",
+			"• Developed a winning hackathon project that could help save over $30,000 per year."
 		],
 	},
 ];
@@ -94,6 +94,15 @@ export const experiences = [
 export const projectsData = [
 	{
 		id: 1,
+		title: 'Ecommerce Website',
+		description: "This is a basic ecommerce website that I developed with two of my brothers. The UI was created with React.js and the products and images were hosted remotely and pulled from an S3 bucket. " +
+			"The website is deployed using Route53 and cloudfront AWS resources. This project was my introduction to using AWS. The website is fully operational and has two products currently listed for purchase.",
+		link: "https://www.euplora.com",
+		linkType: "Website",
+		img: euplora,
+	},
+	{
+		id: 2,
 		title: 'Distributed System Music Streamer',
 		description: "This is a multi-threaded client/server music streaming application. This music streamer runs each client on their own seperate thread as part of a thread pool. " +
 			"These clients are all connected through the server allowing for messaging between clients. This application also has mp3 file uploading functionality giving clients the ability " +
@@ -103,7 +112,16 @@ export const projectsData = [
 		img: musicStreamer,
 	},
 	{
-		id: 2,
+		id: 3,
+		title: 'Iris Classifier',
+		description: "This is a machine learning project that aims to classify the family of iris given instances with 4 features. The features being petal length, petal width, sepal length, and sepal width. " +
+		"I used four different types of classifiers in this project to try and achieve the best results, I used: Perceptron, kNN, Decision Tree, and SVM. The results showed that ",
+		link: "https://github.com/Kolbyw/cs412-FinalProject",
+		linkType: "Github",
+		img: iris,
+	},
+	{
+		id: 4,
 		title: 'Rock Paper Scissors App',
 		description: "This is a basic android application that I developed along with two of my brothers. This game is a basic 'Rock Paper Scissors' style game but it has two different modes. " +
 			"One game mode is 'Easy' where the computer picks a random choice of the three options. In the 'Medium' game mode the computer will analyze your past guesses to predict your choice. " +
@@ -112,121 +130,4 @@ export const projectsData = [
 		linkType: "App",
 		img: rockPaperScissors,
 	},
-	{
-		id: 3,
-		title: 'Ecommerce Website',
-		description: "This is a basic ecommerce website that I developed with two of my brothers. The UI was created with React.js and the products and images were hosted remotely and pulled from an S3 bucket. " +
-			"The website is deployed using Route53 and cloudfront AWS resources. This project was my introduction to using AWS. The website is fully operational and has two products currently listed for purchase.",
-		link: "https://www.euplora.com",
-		linkType: "Website",
-		img: euplora,
-	},
 ];
-
-export const singleProjectData = {
-	ProjectHeader: {
-		title: 'Project Management UI',
-		publishDate: 'Jul 26, 2021',
-		tags: 'UI / Frontend',
-	},
-	ProjectImages: [
-		{
-			id: 1,
-			title: 'Kabul Project Management UI',
-			img: Image1,
-		},
-		{
-			id: 2,
-			title: 'Kabul Project Management UI',
-			img: Image2,
-		},
-		{
-			id: 3,
-			title: 'Kabul Project Management UI',
-			img: Image3,
-		},
-	],
-	ProjectInfo: {
-		ProjectDetailsHeading: 'Challenge',
-		ProjectDetails: [
-			{
-				id: 1,
-				details:
-					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
-			},
-			{
-				id: 2,
-				details:
-					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-			},
-			{
-				id: 3,
-				details:
-					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-			},
-			{
-				id: 4,
-				details:
-					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
-			},
-		],
-		SocialSharingHeading: 'Share This',
-		SocialSharing: [
-			{
-				id: 1,
-				name: 'Twitter',
-				icon: <FiTwitter />,
-				url: 'https://twitter.com/realstoman',
-			},
-			{
-				id: 2,
-				name: 'Instagram',
-				icon: <FiInstagram />,
-				url: 'https://instagram.com/realstoman',
-			},
-			{
-				id: 3,
-				name: 'Facebook',
-				icon: <FiFacebook />,
-				url: 'https://facebook.com/',
-			},
-			{
-				id: 4,
-				name: 'LinkedIn',
-				icon: <FiLinkedin />,
-				url: 'https://linkedin.com/',
-			},
-			{
-				id: 5,
-				name: 'Youtube',
-				icon: <FiYoutube />,
-				url: 'https://www.youtube.com/c/StomanStudio',
-			},
-		],
-	},
-	RelatedProject: {
-		title: 'Related Projects',
-		Projects: [
-			{
-				id: 1,
-				title: 'Mobile UI',
-				img: Image4,
-			},
-			{
-				id: 2,
-				title: 'Web Application',
-				img: Image5,
-			},
-			{
-				id: 3,
-				title: 'UI Design',
-				img: Image6,
-			},
-			{
-				id: 4,
-				title: 'Kabul Mobile App UI',
-				img: Image3,
-			},
-		],
-	},
-};
